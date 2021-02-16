e54ac4cc9493735b405280af1977b4180832ea81
vicentemoraleda <vicente@moraleda.es>
2021-02-16 08:14:43 +0100

    share code on github

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,40 @@
 
-import React from 'react';
-import Producto from './components/Producto';
-import data from './data';
+import React from `react`;
+import Producto from `./components/Producto`;
+import data from `./data`;
 
 function App() {
   return (
 <div className="grid-container">
      <header className="cabecera">
-     <div className="row cabecera">
-           <a  href="/"><img src="img/logo.png"  alt="logo"/></a>
-           <a   href="/">El rastro de Pepa </a>
-     </div>
+              <div className="row cabecera">
+                    <a  href="/"><img src="img/logo.png"  alt="logo"/></a>
+                    <a   href="/">El rastro de Pepa </a>
+              </div>
 
-     <div className="row botonera">
-       <div></div>
-       <div>
-       <a href="/cart" >Carrito</a>
-       <a href="/signin">Alta Comprador</a>
-       </div>
-     </div>   
+              <div className="row botonera">
+                <div></div>
+                <div>
+                <a href="/cart" >Carrito</a>
+                <a href="/signin">Alta Comprador</a>
+                </div>
+              </div>   
      </header>
 
 <main>
   <div>
-<div className="row center"> 
-{data.productos.map((producto)=>(
-<Producto key={producto._id} producto={producto}></Producto>
-  ))}
-</div>
+        <div className="row center"> 
+        {data.productos.map((producto)=>(
+        <Producto key={producto._id} producto={producto}></Producto>
+          ))}
+        </div>
 
-<div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product}></Product>
-            ))}
+          <div className="row center">
+                      {data.products.map((product) => (
+                        <Product key={product._id} product={product}></Product>
+                      ))}
+          </div>
 </div>
-
 
 </main>
 
@@ -46,7 +46,7 @@ Todos los derechos reservados
 
 
 </div> 
-  );
-}
+
+  )}
 
 export default App;
