70dd532a15c3462fbdde43eee821f42d464e4331
vicentemoraleda <vicente@moraleda.es>
2021-02-16 06:43:56 +0100

    share code on github

---
+++ my-app/src/components/Producto.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+export default function Producto(props) {
+    const {producto}=props;
+    return (
+        <div key={producto._id} className="card">
+        <a href={`/producto/${producto._id}?`}>
+            <img className="medium" src={producto.image1} alt={producto.nombre} />
+        </a>
+    <div className="card-body">
+    <a href={`/producto/${producto._id}`}>
+        {producto.nombre}</a>
+
+
+        
+
+
+
+
+        <div className="precio">{producto.precio}€</div>
+        </div>
+    </div>
+    );
+}
---
+++ my-app/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    productos:[
+        {
+            _id:'1',
+            nombre: 'Calculadora',
+            image1: '/img/calculadora1.jpg',
+            categoria: 'antiguedades',
+            descripcion: 'calculadora muy antigua',
+            precio: '80',
+        },
+
+        {
+            _id:'1',
+            nombre: 'Calculadora',
+            image1: '/img/calculadora1.jpg',
+            categoria: 'antiguedades',
+            descripcion: 'calculadora muy antigua',
+            precio: '80',
+        },
+
+        {
+            _id:'1',
+            nombre: 'Calculadora',
+            image1: '/img/calculadora1.jpg',
+            categoria: 'antiguedades',
+            descripcion: 'calculadora muy antigua',
+            precio: '80',
+        },
+
+        {
+            _id:'1',
+            nombre: 'Calculadora',
+            image1: '/img/calculadora1.jpg',
+            categoria: 'antiguedades',
+            descripcion: 'calculadora muy antigua',
+            precio: '80',
+        },
+        
+    ]
+};
+
+export default data;
